78ab06d7669e59afda50b6fd359718cf58beff80
student <sandy891230@gmail.com>
2021-03-12 15:02:17 +0800

    第一次commit 第二次commit 加入dev分支程式碼 加入master分支程式碼

--- app/src/main/java/tw/edu/pu/csim/wang/tzuyu/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/wang/tzuyu/MainActivity.kt
@@ -12,3 +12,19 @@ class MainActivity : AppCompatActivity() {
 
     }
 }
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = “Hello tcyang”
+
+        TestMaster()
+    }
+
+    fun TestMaster(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"
+    }
+}
