c3ac76daa70b7015b52f53e9f4bed288e43b11ab
student <s1080310@gm.pu.edu.tw>
2021-03-19 13:51:19 +0800

    他人加入螢幕觸控方法

--- app/src/main/java/tw/edu/pu/csim/wang/tzuyu/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/wang/tzuyu/MainActivity.kt
@@ -45,3 +45,8 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+override fun onTouchEvent(event: MotionEvent?): Boolean {
+    var txv:TextView = findViewById<TextView>(R.id.txv)
+    txv.text = "tcyangpu修改螢幕觸控"
+    return true
+}
